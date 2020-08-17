86e55d2e0232e2ca4811df8f45b66a5ca256b5dd
thaitq7141 <thaitq7141@co-well.com.vn>
2020-08-17 15:39:01 +0700

    add 3 chart : product , revenue , sku

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import './App.css';
 import RevenueChart from './component/revenue-chart';
+import ProductChart from './component/product-chart';
+import SKUChart from './component/sku-chart';
 function App() {
   return (
-    <RevenueChart />
+    <div>
+      <RevenueChart />
+      <ProductChart />
+      <SKUChart />
+    </div>
+
   );
 }
 export default App;
