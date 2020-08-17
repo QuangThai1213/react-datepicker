525c46592c59bcd246285f1964f7ea97a9bf54e1
thaitq7141 <thaitq7141@co-well.com.vn>
2020-08-17 15:39:01 +0700

    add 3 chart : product , revenue , sku

---
+++ src/common/app-config.js
@@ -0,0 +1,71 @@
+import moment from 'moment';
+function chart_options(data) {
+    return {
+        chart: {
+            type: 'column'
+        },
+        xAxis: {
+            type: 'category',
+            labels: {
+                rotation: -45,
+                style: {
+                    fontSize: '13px',
+                    fontFamily: 'Verdana, sans-serif'
+                }
+            }
+        },
+        yAxis: {
+            min: 0,
+            opposite: true,
+            title: {
+                text: 'VNĐ'
+            }
+        },
+        legend: {
+            enabled: false
+        },
+        tooltip: {
+            pointFormat: 'Net Revenue: <b>{point.y} đ</b>'
+        },
+        series: [{
+            name: 'Net Revenue',
+            data: data,
+            dataLabels: {
+                enabled: true,
+                color: '#FFFFFF',
+                align: 'center',
+                y: 10, // 10 pixels down from the top
+                style: {
+                    fontSize: '10px',
+                    fontFamily: 'Verdana, sans-serif'
+                }
+            }
+        }]
+    }
+}
+
+const range = {
+    "Today": [moment().toDate(), moment().toDate()],
+    "Yesterday": [
+        moment().subtract(1, 'days').toDate(),
+        moment().subtract(1, 'days').toDate(),
+    ],
+    'Last 7 Days': [
+        moment().subtract(6, 'days').toDate(),
+        moment().toDate(),
+    ],
+    'Last 30 Days': [
+        moment().subtract(29, 'days').toDate(),
+        moment().toDate(),
+    ],
+    'This Month': [
+        moment().startOf('month').toDate(),
+        moment().endOf('month').toDate(),
+    ],
+    'Last Month': [
+        moment().subtract(1, 'month').startOf('month').toDate(),
+        moment().subtract(1, 'month').endOf('month').toDate(),
+    ],
+}
+
+export { chart_options, range };
--- src/component/product-chart.js
+++ src/component/product-chart.js
@@ -7,7 +7,7 @@ import Highcharts from 'highcharts';
 import HighchartsReact from 'highcharts-react-official';
 import WooCommerceApi from '../common/woo-config'
 import { chart_options, range } from '../common/app-config'
-class RevenueChart extends React.Component {
+class ProductChart extends React.Component {
     constructor(props) {
         super(props);
         this.ranges = range
@@ -18,21 +18,22 @@ class RevenueChart extends React.Component {
             chartData: []
         };
 
-        this.handleCallback = this.handleCallback.bind(this);
-        this.getData = this.getData.bind(this);
+        this.handleCallback = this.handleCallback.bind(this)
+        this.getData = this.getData.bind(this)
     }
     getData() {
-        WooCommerceApi.get(`reports/products/stats?after=${this.state.startDate}T00:00:00&before=${this.state.endDate}T23:59:59&interval=day&order=asc&per_page=100&_locale=user`).then(response => {
+        WooCommerceApi.get(`reports/products?_locale=user&after=${this.state.startDate}T00:00:00&before=${this.state.endDate}T23:59:59&extended_info=true&order=desc&orderby=items_sold&page=1&per_page=25`).then(response => {
             let data = [];
-            response.data.intervals.forEach(element => {
-                data.push([element.interval, element.subtotals.net_revenue])
+            console.log(response)
+            response.data.forEach(element => {
+                data.push([element.extended_info.name, element.net_revenue])
             });
             this.setState({ chartData: data })
         })
-    };
+    }
     componentDidMount() {
-        this.getData();
-    };
+        this.getData()
+    }
     handleCallback(start, end, label) {
         if (start.format("DD/MM/YYYY").toString() === end.format("DD/MM/YYYY").toString()) {
             this.setState({
@@ -47,8 +48,8 @@ class RevenueChart extends React.Component {
             startDate: start.format("YYYY-MM-DD"),
             endDate: end.format("YYYY-MM-DD"),
         })
-        this.getData();
-    };
+        this.getData()
+    }
     render() {
         return (
             <div>
@@ -72,4 +73,4 @@ class RevenueChart extends React.Component {
         );
     }
 }
-export default RevenueChart; 
+export default ProductChart; 
