eeefb02d532aeda5a8e46b30438c312214c21cb4
thaitq7141 <thaitq7141@co-well.com.vn>
2020-08-17 15:59:40 +0700

    add category chart

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import RevenueChart from './component/revenue-chart';
 import ProductChart from './component/product-chart';
 import SKUChart from './component/sku-chart';
+import CategoryChart from './component/category-chart';
 function App() {
   return (
     <div>
       <RevenueChart />
       <ProductChart />
       <SKUChart />
+      <CategoryChart />
     </div>
 
   );
